6558377707fe4d801bf19096443ea284df65a19d
Mahmoud <samy.m@aicomai.ai>
2025-07-05 14:30:17 +0300

    Checkout logic (route, service, model)

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,8 +1,7 @@
 import express from 'express';
-import { addItemToCart,clearCart,deleteItemIncart,getActiveCartForUser, updateItemInCart} from '../services/cartService';
+import { addItemToCart,checkout,clearCart,deleteItemIncart,getActiveCartForUser, updateItemInCart} from '../services/cartService';
 import validateJWT from '../middlewares/validateJWT';
 import { ExtendRequest } from '../types/extendRequest';
-
 
 
 const router = express.Router();
@@ -39,5 +38,12 @@ router.delete("/items/:productId", validateJWT, async (req: ExtendRequest, res) 
   const response = await deleteItemIncart({ userId, productId });
   res.status(response.statusCode).send(response.data);
 })
+
+router.post('/checkout', validateJWT, async (req: ExtendRequest, res) => {
+    const userId = req?.user?._id;
+    const { address } = req.body;
+    const response = await checkout({ userId, address });
+    res.status(response.statusCode).send(response.data);
+  });
 
 export default router;
