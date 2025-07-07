8f16faac1d7bcd6f9fbc2ca32bbdcc7e99fa3cea
Mahmoud <samy.m@aicomai.ai>
2025-07-07 20:42:35 +0300

    Login Page + Routing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,14 +11,17 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { useAuth } from '../context/Auth/AuthContext';
-
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
-  const { username, token } = useAuth();
+  const { username, isAuthenticated } = useAuth();
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
   );
+
+  const navigate = useNavigate();
 
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
@@ -27,8 +30,10 @@ function Navbar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-  
-  console.log('From navbar', { username, token })
+
+  const handleLogin = () => {
+    navigate('/login')
+  }
 
   return (
     <AppBar position="static">
@@ -55,24 +60,38 @@ function Navbar() {
                 variant="h6"
                 noWrap
                 component="a"
-                
                 sx={{
                   mr: 2,
                   display: { xs: 'none', md: 'flex' },
                   fontFamily: 'monospace',
                   fontWeight: 700,
-                  
                 }}
               >
                 Tech Hub
               </Typography>
             </Box>
-
             <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
+              {isAuthenticated ? (
+                <>
+                  <Tooltip title="Open settings">
+                <Grid
+                  container
+                  alignItems="center"
+                  justifyContent="center"
+                  gap={2}
+                >
+                  <Grid item>
+                    <Typography>{username}</Typography>
+                  </Grid>
+                  <Grid item>
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Avatar
+                        alt={username || ''}
+                        src="/static/images/avatar/2.jpg"
+                      />
+                    </IconButton>
+                  </Grid>
+                </Grid>
               </Tooltip>
               <Menu
                 sx={{ mt: '45px' }}
@@ -90,14 +109,22 @@ function Navbar() {
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
               >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography sx={{ textAlign: 'center' }}>
-                      {setting}
-                    </Typography>
-                  </MenuItem>
-                ))}
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>
+                    My Orders
+                  </Typography>
+                </MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>
+                    Logout
+                  </Typography>
+                </MenuItem>
               </Menu>
+              </>
+              ) : (
+                <Button variant='contained' color='success' onClick={handleLogin}>Login</Button>
+              )}
+              
             </Box>
           </Box>
         </Toolbar>
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -8,10 +8,9 @@ import { BASE_URL } from '../constants/baseUrl';
 import { useAuth } from '../context/Auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
-const RegisterPage = () => {
+const LoginPage = () => {
   const [error, setError] = useState("");
-  const firstNameRef = useRef<HTMLInputElement>(null);
-  const lastNameRef = useRef<HTMLInputElement>(null);
+  
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
@@ -20,36 +19,30 @@ const RegisterPage = () => {
   const { login } = useAuth();
 
   const onSubmit = async () => {
-    const firstName = firstNameRef.current?.value;
-    const lastName = lastNameRef.current?.value;
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
 
     // Validate the from data
-    if(!firstName || !lastName || !email || !password) {
+    if(!email || !password) {
       setError('Check submitted data');
-    }
-
-    if(!firstName || !lastName || !email || !password) {
       return;
     }
 
     // Make the call to API to creat the user
-    const response = await fetch(`${BASE_URL}/user/register`, {
+    const response = await fetch(`${BASE_URL}/user/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        firstName,
-        lastName,
+        
         email,
         password,
       }),
     });
 
     if (!response.ok) {
-      setError("Unable to register user, please try different credientials!");
+      setError("Unable to login user, please try different credientials!");
     }
 
     const token = await response.json();
@@ -75,7 +68,7 @@ const RegisterPage = () => {
           mt: 4,
         }}
       >
-        <Typography variant="h6">Register New Account</Typography>
+        <Typography variant="h6">Login to Your Account</Typography>
         <Box
           sx={{
             display: 'flex',
@@ -86,17 +79,7 @@ const RegisterPage = () => {
             borderColor: '#f5f5f5',
             p: 2,
           }}
-        >
-          <TextField
-            inputRef={firstNameRef}
-            label="Full Name"
-            name="fullName"
-          />
-          <TextField
-            inputRef={lastNameRef}
-            label="last Name"
-            name="lastName"
-          />
+        > 
           <TextField inputRef={emailRef} label="Email" name="email" />
           <TextField
             inputRef={passwordRef}
@@ -105,7 +88,7 @@ const RegisterPage = () => {
             name="password"
           />
           <Button onClick={onSubmit} variant="contained">
-            Register
+            Login
           </Button>
           {error && <Typography sx={{color: "red"}}>{error}</Typography>}
         </Box>
@@ -114,4 +97,4 @@ const RegisterPage = () => {
   );
 };
 
-export default RegisterPage;
+export default LoginPage;
