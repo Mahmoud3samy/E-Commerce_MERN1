d8c6f6276ee22207d8ba80486219962780cef666
Mahmoud <samy.m@aicomai.ai>
2025-07-07 14:12:33 +0300

    All Products in Home page + Integration with API

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -14,6 +14,19 @@ export const seedInitialProducts = async () => {
         price: 15000,
         stock: 10,
       },
+      {
+        title: 'Asus Laptop',
+        image:
+          'https://dlcdnwebimgs.asus.com/gain/7909b0a1-1457-4953-a9e7-41b2be78affa/',
+        price: 25000,
+        stock: 20,
+      },
+      {
+        title: 'HP Laptop',
+        image: 'https://m.media-amazon.com/images/I/41oB3VYKi5L.jpg',
+        price: 40000,
+        stock: 8,
+      },
     ];
   
     const existingProducts = await getAllProducts();
