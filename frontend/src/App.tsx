c37305ea21439ee0cd39576aabfe8ee261573910
Mahmoud <samy.m@aicomai.ai>
2025-07-10 21:59:18 +0300

    checkout integration + order success page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
 import RegisterPage from './pages/RegisterPage';
-
 import LoginPage from './pages/LoginPage';
 import CartPage from './pages/CartPage';
 import ProtectedRoute from './components/ProtectedRoute';
-
 import AuthProvider from './context/Auth/AuthProvider';
 import CartProvider from './context/Cart/CartProvider';
 import CheckoutPage from './pages/CheckoutPage';
+import OrderSuccessPage from './pages/OrderSuccessPage';
 
 function App() {
   return (
@@ -24,6 +23,7 @@ function App() {
             <Route element={<ProtectedRoute />}>
               <Route path="/cart" element={<CartPage />} />
               <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/order-success" element={<OrderSuccessPage />} />
             </Route>
           </Routes>
         </BrowserRouter>
